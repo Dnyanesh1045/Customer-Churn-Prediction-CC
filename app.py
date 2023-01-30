91c96fad20408f91d698cb8dff5bc253389bbc8f
Dnyanesh1045 <kharpude104510@gmail.com>
2023-01-30 15:27:38 +0530

    port number changed

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ def predict():
 
     
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080, debug=False)
 
     
 # return render_template('index.html',result=result)
